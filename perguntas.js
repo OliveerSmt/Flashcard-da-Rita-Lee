197d79a91d780977ec807210a40d2818eb27c75b
OliveerSmt <gf483037@gmail.com>
2024-12-01 00:15:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Vida Pessoal',
+    'Quem é Rita Lee?',
+    'Rita Lee é uma cantora, compositora e ícone do rock brasileiro'
+)
+
+criaCartao(
+    'Discografia',
+    'Qual foi o primeiro álbum de Rita Lee?',
+    'O primeiro álbum de Rita Lee foi "Rita Lee", lançado em 1970'
+)
+
+criaCartao(
+    'Composição',
+    'Qual é o estilo musical de Rita Lee?',
+    'Rita Lee mistura rock, pop, MPB e psicodelia em sua música'
+)
+
+criaCartao(
+    'Rita Lee',
+    'Quais são os maiores sucessos de Rita Lee?',
+    'Alguns dos maiores sucessos de Rita Lee incluem "Ovelha Negra", "Agora Só Falta Você" e "Mania de Você"'
+)
